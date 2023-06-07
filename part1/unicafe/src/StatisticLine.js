9e3d46112885f40af77c73198f21899ad467fdfa
Mrityunjay Kumar <mrityunjaykumar835@gmail.com>
2023-06-07 23:13:18 +0530

    i hava commpleted course information ex1.10

---
+++ part1/unicafe/src/StatisticLine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatisticLine = ({test,value,symbol}) => {
+  return (
+    <div>
+        {test} {value} {symbol}
+    </div>
+  )
+}
+
+export default StatisticLine
--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import StatisticLine from './StatisticLine'
 
 const Statistics = ({good,neutral,bad}) => {
   return (
     <div>
-     good {good}<br/>
-     neutral {neutral}<br/>
-     bad {bad}<br/>
-     all {good+neutral+bad}<br/>
-     average {(good+neutral+bad)/3}<br/>
-     positive {good/(good+neutral+bad)*100} %
-    </div>
+        <StatisticLine  type="good" value={good}/>
+        <StatisticLine  type="neutral" value={neutral}/>
+        <StatisticLine  type="bad" value={bad}/>
+        <StatisticLine  type="all" value={good+neutral+bad}/>
+
+        <StatisticLine  type="average" value={(good-bad)/(good+neutral+bad)}/>
+
+        <StatisticLine  type="positive" value={good/(good+neutral+bad)*100} symbol="%"/> 
+    </div>  
   )
 }
 
