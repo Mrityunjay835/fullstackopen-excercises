813d1f70273b68cc827cf302f3c08783e68171c8
Mrityunjay Kumar <mrityunjaykumar835@gmail.com>
2023-06-10 00:27:51 +0530

    i hava commpleted ex2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App=()=> {
+  const [persons,setPersons]=useState([{ name:'Arto Hellas'}]);
+  const [personName,setPersonName] = useState("fds");
+
+  const addPerson=(event)=>{
+    event.preventDefault();
+    console.log("working form");
+     setPersons(persons.concat({name: personName}));
+  }
+
+  const nameHandler=(event)=>{
+    console.log(event.target.value);
+    setPersonName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name : <input value={personName} onChange={nameHandler}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map((item,index)=>
+          <div key={index}>{item.name}</div>)
+
+      }
+
+    </div>
+  );
+}
+
+export default App;
