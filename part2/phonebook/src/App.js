0db772b6c30678880b763ce440aa6f4725c70b47
Mrityunjay Kumar <mrityunjaykumar835@gmail.com>
2023-06-12 22:58:03 +0530

    i hava commpleted ex2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
 const App=()=> {
-  const [persons,setPersons]=useState([{ name:'Arto Hellas'}]);
+  const [persons,setPersons]=useState([{ name:'Arto Hellas',number: "123-34-32435325" }]);
   const [personName,setPersonName] = useState("fds");
+  const [number,setNumber] = useState("");
 
   const addPerson=(event)=>{
     event.preventDefault();
     console.log("working form");
-    persons.find(item=>item.name===personName)?alert(`${personName} is already added to phonebook`):setPersons(persons.concat({name: personName}));
+    persons.find(item=>item.name===personName)?alert(`${personName} is already added to phonebook`):setPersons(persons.concat({name: personName,
+    number:number}));
   }
 
   const nameHandler=(event)=>{
     console.log(event.target.value);
     setPersonName(event.target.value);
+  }
+
+  const numberHandler=(event)=>{
+    console.log(event.target.value);
+    setNumber(event.target.value);
   }
 
 
@@ -25,6 +32,9 @@ const App=()=> {
           name : <input value={personName} onChange={nameHandler}/>
         </div>
         <div>
+          number : <input value={number} onChange={numberHandler}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
 
@@ -32,7 +42,7 @@ const App=()=> {
       <h2>Numbers</h2>
       {
         persons.map((item,index)=>
-          <div key={index}>{item.name}</div>)
+          <div key={index}>{item.name} {item.number}</div>)
 
       }
 
